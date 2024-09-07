24f0004d96d69511522a283de19e1b94dd8188f7
OlgaPython71 <kuznetsova-o@mail.ru>
2024-09-07 19:28:01 +0300

    Update module_5_3.py

--- module_5_3.py
+++ module_5_3.py
@@ -17,32 +17,40 @@ class House:
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
     def __eq__(self, other):
-        return self.number_of_floors == isinstance(other.number_of_floors, int)
+        isinstance(other, House)
+        return self.number_of_floors == other.number_of_floors
 
     def __lt__(self, other):
+        isinstance(other, House)
         return self.number_of_floors < other.number_of_floors
 
     def __le__(self, other):
+        isinstance(other, House)
         return self.number_of_floors <= other.number_of_floors
 
     def __gt__(self, other):
+        isinstance(other, House)
         return self.number_of_floors > other.number_of_floors
 
     def __ge__(self, other):
+        isinstance(other, House)
         return self.number_of_floors >= other.number_of_floors
 
     def __ne__(self, other):
+        isinstance(other, House)
         return self.number_of_floors != other.number_of_floors
 
     def __add__(self, value):
+        isinstance(value, int)
         self.number_of_floors += value
         return self
 
     def __radd__(self, value):
-        return self.__add__(self)
+        isinstance(value, int)
+        self.__add__(value)
 
-    def __iadd__(self, value):
-        return self.__add__(self)
+    # def __iadd__(self, value):
+    #     return self.__add__(self)
 
 
 h1 = House('ЖК Эльбрус', 10)
@@ -57,9 +65,9 @@ h1 = h1 + 10  # __add__
 print(h1)
 print(h1 == h2)
 
-h1 += 10  # __iadd__
-print(h1)
-
+# h1 += 10  # __iadd__
+# print(h1)
+#
 h2 = 10 + h2  # __radd__
 print(h2)
 
