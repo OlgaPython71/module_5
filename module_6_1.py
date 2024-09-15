c09e315a0d6e3f0140b773f2eea3445be7ee2557
OlgaPython71 <kuznetsova-o@mail.ru>
2024-09-15 17:38:59 +0300

    Update module_6_1.py

--- module_6_1.py
+++ module_6_1.py
@@ -6,12 +6,11 @@ class Animal:
         self.name = name
 
     def eat(self, food):
-        food = Plant()
-        if Plant().edible == True:
-            print(f'{self.name} съел {food}')
+        if food.edible == True:
+            print(f'{self.name} съел {food.name}')
             self.fed = True
         else:
-            print(f'{self.name} не стал есть {food}')
+            print(f'{self.name} не стал есть {food.name}')
             self.alive = False
 
 
