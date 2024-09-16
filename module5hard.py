e9d7c135efc9971ae75826f0e532265005ca3e57
OlgaPython71 <kuznetsova-o@mail.ru>
2024-09-16 16:21:50 +0300

    Update module5hard.py

--- module5hard.py
+++ module5hard.py
@@ -9,20 +9,39 @@ class User:
         return self.nickname
 
     def __hash__(self):
-        return self.password
+        return hash(self.password)
 
     def __int__(self):
         return self.age
 
 class Video:
+    time_now = 0
 
-    def __init__(self, title, duration, time_now, adult_mode):
+    def __init__(self, title, duration, adult_mode=False):
         self.title = title
         self.duration = duration
-        self.time_now = time_now
-        self.adult_mode = adult_mode
 
     def __str__(self):
         return self.title
 
-    def 
+class UrTube:
+    def __init__(self):
+        self.users = []
+        self.videos = []
+
+    def add(self, i, Video):
+        if i != Video:
+            self.videos.append(i)
+
+
+
+
+ur = UrTube()
+v1 = Video('Лучший язык программирования 2024 года', 200)
+v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+
+# Добавление видео
+ur.add(v1, v2)
+print(ur)
+print(v1)
+print(v2)
